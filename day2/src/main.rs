1c4fcf19bf749f64c6766f86a8db13e29081b7e1
Vadym S. Khondar <vadym@khondar.name>
2024-12-06 18:36:05 +0000

    Add solution for day2:2.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,17 +1,46 @@
 use std::{
+    cmp::{max, Ordering},
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
 };
 
-fn is_safe_report(report: &[u32]) -> bool {
-    let mut last_level = report[0];
-    let ascending = report[0] < report[1];
-    for &curr_level in report.iter().skip(1) {
-        if (ascending && curr_level < last_level) || (!ascending && curr_level > last_level) {
-            return false;
-        }
+fn determine_ordering(report: &[u32]) -> Ordering {
+    let counts = report.windows(2).fold(
+        HashMap::from([
+            (Ordering::Less, 0),
+            (Ordering::Equal, 0),
+            (Ordering::Greater, 0),
+        ]),
+        |mut acc, el| {
+            acc.entry(match (el[0], el[1]) {
+                (f, s) if f < s => Ordering::Less,
+                (f, s) if f > s => Ordering::Greater,
+                _ => Ordering::Equal,
+            })
+            .and_modify(|v| *v += 1);
 
-        if !(1..=3).contains(&last_level.abs_diff(curr_level)) {
+            acc
+        },
+    );
+
+    counts
+        .iter()
+        .max_by_key(|(_, &count)| count)
+        .map(|(&order, _)| order)
+        .unwrap()
+}
+
+fn is_safe_report(report: &[u32]) -> bool {
+    let ordering = determine_ordering(report);
+    let mut last_level = report[0];
+
+    for &curr_level in report.iter().skip(1) {
+        if (ordering == Ordering::Less && curr_level <= last_level)
+            || (ordering == Ordering::Greater && curr_level >= last_level)
+            || ordering == Ordering::Equal
+            || !(1..=3).contains(&last_level.abs_diff(curr_level))
+        {
             return false;
         }
 
@@ -22,16 +51,32 @@ fn is_safe_report(report: &[u32]) -> bool {
 }
 
 fn is_safe_report_dampened(report: &[u32]) -> bool {
+    let ordering = determine_ordering(report);
     let mut last_level = report[0];
-    let ascending = report[0] < report[1];
-    for (idx, &curr_level) in report.iter().enumerate().skip(1) {
-        if (ascending && curr_level < last_level) || (!ascending && curr_level > last_level) {
-            return is_safe_report(&[&report[0..idx], &report[idx + 1..]].concat());
-        }
 
+    for (idx, &curr_level) in report.iter().enumerate().skip(1) {
         let diff = last_level.abs_diff(curr_level);
-        if !(1..=3).contains(&diff) {
-            return is_safe_report(&[&report[0..idx], &report[idx + 1..]].concat());
+
+        if (ordering == Ordering::Less && curr_level <= last_level)
+            || (ordering == Ordering::Greater && curr_level >= last_level)
+            || ordering == Ordering::Equal
+            || !(1..=3).contains(&diff)
+        {
+            // println!("Unsafe without dampening: {:?}", report);
+
+            let try_splits = [max(0, idx - 1), idx];
+
+            for split in try_splits {
+                let try_fixed_report = [&report[0..split], &report[split + 1..]].concat();
+                let is_safe_now = is_safe_report(&try_fixed_report);
+
+                if is_safe_now {
+                    // println!("     But dampening helps: {:?}", try_fixed_report);
+                    return is_safe_now;
+                }
+            }
+
+            return false;
         }
 
         last_level = curr_level;
@@ -44,6 +89,7 @@ fn main() {
     let input_file = File::open("input.txt").expect("Failed to open file.");
     let reader = BufReader::new(input_file);
 
+    let mut total_reports_count = 0;
     let mut safe_reports_count = 0;
     let mut safe_reports_count_dampened = 0;
     for line in reader.lines() {
@@ -56,14 +102,12 @@ fn main() {
         let is_safe = is_safe_report(&report);
         let is_safe_dampened = is_safe_report_dampened(&report);
 
-        if is_safe != is_safe_dampened {
-            println!("Dampening helps: {:?}", report);
-        }
-
         safe_reports_count += is_safe as u32;
         safe_reports_count_dampened += is_safe_dampened as u32;
+        total_reports_count += 1;
     }
 
+    println!("Total reports count: {}", total_reports_count);
     println!(
         "Number of safe reports without dampening: {}",
         safe_reports_count
@@ -76,7 +120,9 @@ fn main() {
 
 #[cfg(test)]
 mod test {
-    use crate::{is_safe_report, is_safe_report_dampened};
+    use std::cmp::Ordering;
+
+    use crate::{determine_ordering, is_safe_report, is_safe_report_dampened};
 
     #[test]
     fn is_safe_report_works() {
@@ -86,6 +132,10 @@ mod test {
         assert!(!is_safe_report(&[1, 3, 2, 4, 5]));
         assert!(!is_safe_report(&[8, 6, 4, 4, 1]));
         assert!(is_safe_report(&[1, 3, 6, 7, 9]));
+
+        assert!(!is_safe_report(&[1, 1, 2, 3, 4]));
+        assert!(!is_safe_report(&[71, 69, 70, 71, 72, 75]));
+        assert!(!is_safe_report(&[2, 5, 4, 3, 2]));
     }
 
     #[test]
@@ -99,5 +149,17 @@ mod test {
 
         assert!(!is_safe_report_dampened(&[61, 64, 67, 66, 68, 67]));
         assert!(is_safe_report_dampened(&[75, 72, 71, 68, 66, 65, 62, 56]));
+        assert!(is_safe_report_dampened(&[1, 1, 2, 3, 4]));
+        assert!(is_safe_report_dampened(&[2, 5, 4, 3, 2]));
+        assert!(is_safe_report_dampened(&[71, 69, 70, 71, 72, 75]));
+        assert!(is_safe_report_dampened(&[5, 8, 4, 3, 2, 1]));
+    }
+
+    #[test]
+    fn determine_ordering_works() {
+        assert_eq!(determine_ordering(&[1, 2, 3, 4, 5]), Ordering::Less);
+        assert_eq!(determine_ordering(&[5, 4, 3, 2, 1]), Ordering::Greater);
+        assert_eq!(determine_ordering(&[1, 1, 1, 1, 1]), Ordering::Equal);
+        assert_eq!(determine_ordering(&[1, 2, 3, 2, 1, 0]), Ordering::Greater);
     }
 }
