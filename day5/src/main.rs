26fcdf412608a136ff2fb6e5dcbb5c0efa0c4fda
Vadym S. Khondar <vadym@khondar.name>
2024-12-09 22:35:27 +0000

    Add solution for day5:2.

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -26,6 +26,54 @@ impl Rule {
 
         !matches!((first_pos, second_pos), (Some(f), Some(s)) if f > s)
     }
+
+    ///
+    /// Applies current rule to the passed update, as necessary.
+    ///
+    /// Returns:
+    ///  - true if update violated the rule
+    ///  - false if rule was already satisfied and no action was necessary
+    fn satisfy_for_update(&self, update: &mut [u32]) -> bool {
+        let first_pos = update.iter().position(|el| *el == self.0);
+        let second_pos = update.iter().position(|el| *el == self.1);
+
+        if let (Some(f), Some(s)) = (first_pos, second_pos) {
+            if f > s {
+                // println!("Rule {}|{} not satisfied for update: {:?}", self.0, self.1, update);
+                // if order is wrong, swap
+                update.swap(first_pos.unwrap(), second_pos.unwrap());
+                // println!("                     After swapping: {:?}", update);
+
+                return true;
+            }
+        }
+
+        false
+    }
+}
+
+#[allow(dead_code)]
+fn is_correctly_ordered(rules: &[Rule], update: &[u32]) -> bool {
+    rules.iter().all(|r| r.is_satisfied_for_update(update))
+}
+
+fn oder_update(rules: &[Rule], update: &mut [u32]) -> bool {
+    let mut required_reordering = false;
+    loop {
+        let mut ordered = true;
+        for rule in rules {
+            ordered &= !rule.satisfy_for_update(update);
+            if !ordered {
+                required_reordering = true;
+            }
+        }
+
+        if ordered {
+            break;
+        }
+    }
+
+    required_reordering
 }
 
 fn main() {
@@ -34,7 +82,8 @@ fn main() {
 
     let mut rules = Vec::new();
     let mut header_read = true;
-    let mut middle_pages_sum = 0;
+    let mut correctly_ordered_update_middle_pages_sum = 0;
+    let mut incorrectly_ordered_update_middle_pages_sum = 0;
     for line_result in reader.lines() {
         if header_read {
             let rule_line = line_result.expect("Failed to read from the input file.");
@@ -44,10 +93,10 @@ fn main() {
                 continue;
             }
 
-            rules.push(
-                Rule::parse(rule_line.as_str())
-                    .unwrap_or_else(|_| panic!("Failed to parse rule: {}", rule_line)),
-            );
+            let rule = Rule::parse(rule_line.as_str())
+                .unwrap_or_else(|_| panic!("Failed to parse rule: {}", rule_line));
+
+            rules.push(rule);
         } else {
             let update_line = line_result.expect("Failed to read from the input file.");
 
@@ -59,14 +108,28 @@ fn main() {
                 })
                 .collect();
 
-            let update = update_result.unwrap_or_else(|e| panic!("Failed to parse update line '{}': {}", update_line, e));
+            let mut update = update_result
+                .unwrap_or_else(|e| panic!("Failed to parse update line '{}': {}", update_line, e));
 
-            if rules.iter().all(|r| r.is_satisfied_for_update(update.as_slice())) {
-                assert!(update.len() % 2 == 1, "Update {:?} has even number of pages, can't determine the middle page number.", update);
+            assert!(
+                update.len() % 2 == 1,
+                "Update {:?} has even number of pages, can't determine the middle page number.",
+                update
+            );
 
-                middle_pages_sum += update[update.len() / 2];
+            if oder_update(&rules, &mut update) {
+                incorrectly_ordered_update_middle_pages_sum += update[update.len() / 2];
+            } else {
+                correctly_ordered_update_middle_pages_sum += update[update.len() / 2];
             }
         }
     }
-    println!("Sum of middle page numbers of right ordered updates: {}", middle_pages_sum);
+    println!(
+        "Sum of middle page numbers of correctly ordered updates: {}",
+        correctly_ordered_update_middle_pages_sum
+    );
+    println!(
+        "Sum of middle page numbers of incorrectly ordered updates (after fixing): {}",
+        incorrectly_ordered_update_middle_pages_sum
+    );
 }
